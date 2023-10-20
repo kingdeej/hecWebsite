6f9f802eb9cff4d880e455aa099040aab18076ab
kingdeej <devonhenry10196@gmail.com>
2023-10-20 14:56:39 -0500

    main

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,21 +8,22 @@ export default function Hero() {
   return (
     <section className='hero'>
       <div className="hero-wrapper | page-inline-padding flex">
-        <div className="left-hero">
+        <div className="left-hero | flex-column">
           <span className='eyebrow'>Featured Events</span>
-          <h1 className="hero-header | primary-header">
+          <h1 className="hero-header heading-line-style | primary-header">
             Pelican Party
           </h1>
           <ul className="event-info-wrapper">
             <li className='flex'><img src={locationIcon1} alt="location" />Ocho Rios</li>
             <li className='flex'><img src={dateIcon1} alt="date" />Sat July 25</li>
           </ul>
-          <div className="hero-button-wrapper">
-            <button className="button | primary-button">Get Tickets</button>
-            <div className="price-wrapper"><img src={ticketIcon} alt="ticket" /></div>
-            $500
+          <div className="hero-button-wrapper | vertical-align">
+            <button className="button | primary-button ">Get Tickets</button>
+            <div className="price-wrapper | flex"><img src={ticketIcon} alt="ticket" />
+            <p>$500</p>
+            </div>
           </div>
-          <p>Get exclusive tickets here and also purchase food and drinks.</p>
+          <p className='base-text'>Get exclusive tickets here and also purchase food and drinks.</p>
         </div>
         <div className="right-hero">
           <div className="hero-img-cont">
