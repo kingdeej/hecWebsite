9f5b4ed5975480e534ca58c85eb5d486c44ffd01
kingdeej <devonhenry10196@gmail.com>
2023-10-25 13:23:04 -0500

    main

--- src/components/AllEvents.js
+++ src/components/AllEvents.js
@@ -74,30 +74,31 @@ export default class HotEvents extends Component {
 
   ]
   render() {
-    console.log(this.events[this.props.eventAmount]);
     return (
       <section className= {`${this.props.eventStyle} hot-events | page-block-padding`}>
         <div className="hot-events-wrapper | page-inline-padding">
             <div className="hot-events-header | space-between">
                 <h4 className='secondary-header'>{this.props.FlyersType} Events</h4>
-                <a href='/seeAll' className='primary-paragraph'>{this.props.FlyersType}  all</a>
+                <a href='/seeAll' className='primary-paragraph'>See all</a>
             </div>
             <ul className="flyers-wrapper | flex">
               {this.events.map((x, key)=>{
-                return(
-                  <li key={key} className="flyer">
-                      <img src={x.eventImg} className='flyer-img' alt="flyer" />
-                      <div className="flyer-info-cont | flex-center">
-                          <h4 className='flyer-header | heading-line-style'>{x.eventName}</h4>
-                          <ul className="event-info-wrapper">
-                              <li className='flex'><img src={locationIcon} alt="location" />{x.eventLocation}</li>
-                              <li className='flex'><img src={dateIcon} alt="date" />{x.eventDate}</li>
-                          </ul>
-                          <p className='ticket-wrapper flex-center'><img src={ticketIcon} alt="ticket-img" />${x.eventPrice}</p>
-                          <button className="get-tickets-button | button">Get Tickets</button>
-                      </div>
-                  </li>
-                )
+                if (key < this.props.eventAmount) {
+                  return(
+                    <li key={key} className="flyer">
+                        <img src={x.eventImg} className='flyer-img' alt="flyer" />
+                        <div className="flyer-info-cont | flex-center">
+                            <h4 className='flyer-header | heading-line-style'>{x.eventName}</h4>
+                            <ul className="event-info-wrapper">
+                                <li className='flex'><img src={locationIcon} alt="location" />{x.eventLocation}</li>
+                                <li className='flex'><img src={dateIcon} alt="date" />{x.eventDate}</li>
+                            </ul>
+                            <p className='ticket-wrapper flex-center'><img src={ticketIcon} alt="ticket-img" />${x.eventPrice}</p>
+                            <button className="get-tickets-button | button">Get Tickets</button>
+                        </div>
+                    </li>
+                  )
+                }
               })}
             </ul>
         </div>
