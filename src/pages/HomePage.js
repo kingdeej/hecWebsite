99e8dfc375b9420aa66617a7f844aabf3c5bd1a4
kingdeej <devonhenry10196@gmail.com>
2024-03-31 14:56:47 -0500

    main

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Hero from '../components/Hero'
 import Testimonials from '../components/Testimonials'
 import PromoteCta from '../components/PromoteCta'
@@ -6,12 +6,17 @@ import AllEvents from '../components/AllEvents'
 import HomeScreenPopup from '../components/HomeScreenPopup'
 
 export default function HomePage() {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    // return () => {
+    //     document.body.style.overflow = "scroll"
+    // };
+}, []);
   return (
     <main>
         <Hero />
-        <div className='position-center'>
-        <HomeScreenPopup />
-
+        <div className='home-screen-popup-container'>
+          <HomeScreenPopup />
         </div>
         <Testimonials />
         <AllEvents eventAmount={4} eventStyle='hot-event-style' FlyersType='Hot'/>
